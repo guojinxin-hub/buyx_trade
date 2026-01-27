a13e36960d8cd6a0eb8421540a9c193e60e47819
thiswhy <1414770848@qq.com>
2026-01-27 09:40:33 +0800

    新增测试返回值

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -14,23 +14,6 @@ let client = new GateApi.ApiClient();
 
 export const testAPI = async (req, res) => {
     try {
-        const options = await UserTradeOptionsModel.find({
-            isActive: true,
-            isDelete: false,
-            belong: "Gate"
-        }).lean()
-        for (const option of options) {
-            // if (!isEmpty(option)) {
-            //     const {apiKey, apiSecret, isTestOption, currency} = option
-            //     console.log(decrypt("U2FsdGVkX18AxYfDBjxnX/oVfkk8TGWT3/28BZnvW04sNW2RTBmGWxX0J4zsOPSbW80sgaSnd/qVYiSHBJlD3w=="),isTestOption)
-            //     client.setApiKeySecret(decrypt("U2FsdGVkX18AxYfDBjxnX/oVfkk8TGWT3/28BZnvW04sNW2RTBmGWxX0J4zsOPSbW80sgaSnd/qVYiSHBJlD3w=="), decrypt("U2FsdGVkX1+HIBs+7a+pMwNx8Evev87z3fnC4/yepnVOefeLJEv/a7Z8KYe5I0aubykz9poeg8lbPSPW7Am1wW47OByAYcRVu9v8m4PIhFIsTAwJhkMizYwFuSjSc4R8"));
-            //     client.basePath = isTestOption ? TRADE_TEST_API_URL : TRADE_API_URL
-            //     const futuresApi = new GateApi.FuturesApi(client);
-            //     const futureAccount = await futuresApi.listFuturesAccounts('usdt')
-            //     console.log(futureAccount)
-            //     saveUserBalance(option.userId, futureAccount.body)
-            // }
-        }
         return formatResponse(res, 200, 0, {}, 'success')
         // testCode()
     } catch (e) {
