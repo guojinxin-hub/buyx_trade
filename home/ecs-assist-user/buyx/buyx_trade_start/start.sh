dcfdbcf8e83c2db01d8899d7120e622863a681fa
Benedict <11203939+guojinxin001@user.noreply.gitee.com>
2026-03-15 21:42:04 +0800

    有问题的方案删除掉

--- home/ecs-assist-user/buyx/buyx_trade_start/start.sh
+++
@@ -1,6 +0,0 @@
-# 在拉取代码后，确保进入目录并安装依赖
-cd buyx_trade || exit 1
-// 添加: 使用 npm 安装，避免 yarn 问题
-echo "Installing dependencies with npm..."
-npm config set registry https://registry.npmmirror.com
-npm install --registry=https://registry.npmmirror.com
