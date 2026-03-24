2410395cbe6011d58a5e5a114a6f37a9543f6446
Benedict <11203939+guojinxin001@user.noreply.gitee.com>
2026-03-24 20:17:30 +0800

    提交

--- dataUtils/profitProtection.js
+++ dataUtils/profitProtection.js
@@ -328,10 +328,10 @@ export const handleProfitProtection = async (req, res) => {
 
         // 逐个处理每个用户的盈利保护
         const results = [];
-        for (const userOption of usersWithProfitProtection) {
-            // const userResult = await handleUserProfitProtection(userOption);
-            results.push({ userId: userOption.userId, ...userResult });
-        }
+        // for (const userOption of usersWithProfitProtection) {
+        //     const userResult = await handleUserProfitProtection(userOption);
+        //     results.push({ userId: userOption.userId, ...userResult });
+        // }
 
         // 返回处理结果
         return res.status(200).json({
