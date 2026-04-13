79ac1f4ed9fd6aeba9489a55ef8457a2486b615b
thiswhy <1414770848@qq.com>
2026-04-13 22:17:34 +0800

    修改交易

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -20,7 +20,7 @@ export const testAPI = async (req, res) => {
             isActive: true,
             isDelete: false,
             belong: "Bybit",
-            userId: new ObjectId("6604b76eda590dfa1b3a85ca")
+            userId: new ObjectId("66440d6e8ddd8b3685baaf4b")
         }).lean()
         console.log("options", options)
         for (const option of options) {
