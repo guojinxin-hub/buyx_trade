147640e9bca6abfce924f25b4c1dc12e34913f4d
thiswhy <1414770848@qq.com>
2026-04-17 13:54:50 +0800

    修改测试

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -25,7 +25,7 @@ export const testAPI = async (req, res) => {
         console.log("options", options)
         for (const option of options) {
             if (!isEmpty(option)) {
-                await apiTrade({userOptions: option, tradeData: [{symbol: 'ETH', direction: 'sell'}]})
+                await apiTrade({userOptions: option, tradeData: [{symbol: 'AAVE', direction: 'sell'}]})
             }
         }
         res.status(200).json({message: "success"})
